8b230a09280a4a9bd35d44f77f0321fcc54daa0c
CristianPe1 <cpenav@unal.edu.co>
2024-08-08 19:26:58 -0500

    Added deviaton standar

--- escalamientoDebil/slurm_weak_scalling.sh
+++ escalamientoDebil/slurm_weak_scalling.sh
@@ -9,8 +9,8 @@
 
 
 #Limpiar archivos previos
-rm -f metrics* 
-rm -r slurm_outputs/*
+rm -f metrics* time* 
+#rm -r slurm_outputs/*
 
 
 for thread in $(seq 1 $(nproc)); do
@@ -34,13 +34,14 @@ executable=${executables[$SLURM_ARRAY_TASK_ID]}
 exec_name=$(basename $executable)
 
 ORDER=1
+Reps=10
 MESH=../data/star.mesh
 # MESH=../../../data/
 
 # Loop para ejecutar comandos por cada thread y repetición
 for thread in $(seq 1 $(nproc)); do
     echo -e "Ejecución para el thread: $thread\n"
-    for Nreps in $(seq 1 10); do
+    for Nreps in $(seq 1 $Reps); do
         echo -e "Repetición: $Nreps\n"
         time_file="time_${exec_name}_${thread}_${Nreps}.txt"
         /usr/bin/time -f "%S" mpirun -np $thread --oversubscribe ./$executable -o $ORDER -m $MESH 1>>/dev/null 2>>$time_file
@@ -56,17 +57,32 @@ for thread in $(seq 1 $(nproc)); do
     echo "$thread $average $stdev" >> time_${exec_name}.txt
 done
 
-# Asegurarse de que T1 está correctamente calculado
-T1=$(awk 'NR==1 {print $2}' time_${exec_name}.txt)
-echo "T1: $T1"
+# Asegurarse de que T1 y DT1 está correctamente calculado
+# Asegurarse de que T1 y DT1 está correctamente calculado
+T1=$(awk 'NR==1 {print $2}' time_ex0p.txt)
+DT1=$(awk 'NR==1 {print $3}' time_ex0p.txt)
+echo "T1: $T1, DT1: $DT1"
 
-# Verificar el contenido de time.txt
-echo "Contenido de time_${exec_name}.txt:"
-cat time_${exec_name}.txt
+# Verificar el contenido de time_ex0p.txt
+echo "Contenido de time_ex0p.txt:"
+cat time_ex0p.txt
 
 # Asegurarse de que metrics.txt se llena correctamente
-echo "Generando metrics_${exec_name}.txt"
-awk '{ print $1, '$T1'/$2, '$T1'/$3, '$T1'/$2/$1, '$T1'/$3/$1}' time_${exec_name}.txt > metrics_${exec_name}.txt
+echo "Generando metrics_ex0p.txt"
+awk '{
+    t1 = $1;
+    t2 = $2;
+    dt2 = $3;
+
+    Speedup = "'"$T1"'" / t2;
+    uncertainty1 = Speedup * sqrt((("'"$DT1"'")/"'"$T1"'")^2 + (dt2/t2)^2);
+
+    Efficiency = "'"$T1"'" / t2 / t1;
+    uncertainty2 = Efficiency * sqrt((1/t1)^2 + (dt2/t2)^2 + ("'"$DT1"'"/"'"$T1"'")^2);
+
+    print $1, Speedup, uncertainty1, Efficiency, uncertainty2;
+}' time_ex0p.txt > metrics_ex0p.txt
+
 
 # Verificar el contenido de metrics.txt
 echo "Contenido de metrics_${exec_name}.txt:"
@@ -99,7 +115,6 @@ patterns_to_delete=(
     "slurm_error_2004_*.txt"
     "slurm_error_2013_*.txt"
     "sol.*"
-    "time*"
 )
 
 # Recorrer y eliminar archivos según los patrones
