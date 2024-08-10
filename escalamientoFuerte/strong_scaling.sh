bd625932b613f653c5d3807ae5b98625d3833cdc
Diego <dcamposm@unal.edu.co>
2024-08-10 16:56:11 -0500

    Update bash

--- escalamientoFuerte/strong_scaling.sh
+++ escalamientoFuerte/strong_scaling.sh
@@ -8,18 +8,28 @@
 # parallel "mpirun -np 1 ex1p -m ../data/star-surf.mesh -o {}" ::: 1 2 2>./output/output_${i}.txt
 # done
 
+# mpirun -np 1 ex39p -o 6 2>./output/output_6.txt
 
-ORDER=$(seq 1 2)
 
-REPS=$(seq 1 5)
+# ORDER=$(seq 1 30)
 
-for orden in $ORDER
+# REPS=$(seq 1 10)
+
+ORDER=$(seq 6 6)
+
+REPS=$(seq 1 4)
+
+echo $REPS
+
+for orden in $ORDER;
 do
 echo "$orden ----------------------------------------"
-parallel -N0 "mpirun -np 1 ex1p -m ../data/star.mesh -o $orden" ::: $REPS 2>./output/output_${orden}.txt
+# parallel -N0 "stress -t 10 -c 1" ::: $REPS 
+# parallel -N0 "mpirun -np 1 ex39p -o $orden" ::: $REPS 2>./output/output_${orden}.txt
+seq 10 | parallel -j 10 "mpirun -np 1 ex39p -o $orden 2>>./output/output_$orden.txt"
 done
 
-python3 plot.py
+# python3 plot.py
 
 #rm ./output/output_*.txt
 
