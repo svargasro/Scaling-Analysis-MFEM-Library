139d79227a8d03282180a0ac7638d6f871a7163b
svargasro <svargasro@unal.edu.co>
2024-08-13 23:12:24 -0500

    strongScalingSala29

--- escalamientoFuerte/plot.py
+++ escalamientoFuerte/plot.py
@@ -15,24 +15,25 @@ timeDesv = np.zeros(cantidadArchivos)
 size = np.zeros(cantidadArchivos)
 
 
+
 for i, archivo in enumerate(archivos):
 
     timeArray, sizeArray = np.genfromtxt(f'./output/{archivo}',delimiter=' ', usecols=(0,1),unpack=True)
 
     meanTime = np.mean(timeArray)
+    if (i==0):
+        time0 = meanTime
 
-    time[i] = meanTime
+    time[i] = meanTime/time0
+    timeDesv[i] = np.std(timeArray/meanTime)
 
     size[i] = sizeArray[0]
 
-
-
 #Se normaliza el promedio y desviación estándar dividiendo por el tiempo y desviación estándar que tomó para el índice 0.
-time = time/time[0]
-timeDesv  = np.std(timeArray)
 
 
-errorbar= 3.0*timeDesv
+print(timeDesv)
+errorbar= 10*timeDesv
 
 plt.style.use('ggplot')
 
@@ -40,7 +41,7 @@ fig, axes = plt.subplots(1, 1, figsize=(7, 6))
 
 
 #Se grafica el tiempo de ejecución normalizado vs. tamaño de la matriz.
-axes.errorbar(size, time, yerr= errorbar, fmt='go', ecolor='black', markersize=6, label="Puntos con barras de error.")
+axes.errorbar(size, time, yerr= errorbar, fmt='go', ecolor='black', markersize=2, label="Puntos con barras de error. Errorbar=10*std")
 
 #Se ajustan demás detalles del gráfico.
 axes.set_xlabel('Número de incógnitas en el sistema de ecuaciones.', fontsize=12)
@@ -53,5 +54,5 @@ axes.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 
 plt.tight_layout()
 
-plt.savefig(f'EscalamientoFuerte10Iter.png')
+plt.savefig(f'EscalamientoFuerteSala29_np6.png')
 #plt.show()
