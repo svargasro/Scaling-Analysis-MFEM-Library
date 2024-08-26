862ff7634a6f367fd328cb505af646b7fd0b9e86
svargasro <svargasro@unal.edu.co>
2024-08-26 13:05:04 -0500

    strong and weak scaling working on ex1p with scaling.sh

--- escalamientoFuerte/scaling.sh
+++ escalamientoFuerte/scaling.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-#cd ./escalamientoDebil
-# #Ejecuta escalamiento débil para los ejemplos indicando: (ejecutable orden #threads #repetciciones)
-# bash weak_exp.sh ex1p 2 4 3   #Ejemplo 1 version paralela
-# bash weak_exp.sh ex39p 2 4 3  #Ejemplo 39 version paralela
-#/escalamientoDebil/weak_volta.sh
-
-#Construir gráfica de uso de memoria RAM
-#bash monitor_memoria.sh ex1p 2 1 0.001
-
-
+#Para facilidad de ejecución.
 #Ejecuta el .sh del escalamiento fuerte: ./strong_exp.sh Ejecutable MáximoOrden Threads Repeticiones
 
 ./strong_exp.sh ex1p 2 3 3 #Escalamiento fuerte ex1p
--- escalamientoFuerte/strong_exp.sh
+++ escalamientoFuerte/strong_exp.sh
@@ -17,19 +17,18 @@ timeOutput=resultados/time_${TARGET}_threads_${THREADS}_reps_${MAX_REPS}.csv
 
 # Loop para ejecutar comandos
 echo -e "Escalamiento Fuerte del ejemplo: ${TARGET}. $THREADS thread(s). $MAX_REPS repeticiones.\n"
-rm ./resultados/*.csv
-rm ./resultados/graficas/*
-
+# rm ./resultados/*.csv
+# rm ./resultados/graficas/*
 for orden in $ORDER; do
     echo -e "Ejecucion para el orden: $orden\n"
     echo -n "Orden_$orden," >> $timeOutput
     for Nreps in $REPS; do
         echo -e "Repeticion: $Nreps\n"
         if [ "$Nreps" -ne "$(echo "$REPS" | tail -n 1)" ]; then
-	        resultado=$(mpirun -np $THREADS --oversubscribe ./${TARGET} -o $orden 2>&1 >/dev/null | tail -n 1) #Enviar stdout a /dev/null y el stderr a la variable
+	        resultado=$(mpirun -np $THREADS --oversubscribe ./../cpp_y_ejecutables/${TARGET} -o $orden 2>&1 >/dev/null | tail -n 1) #Enviar stdout a /dev/null y el stderr a la variable
 	        echo -n "$resultado," >> $timeOutput
         else
-            resultado=$(mpirun -np $THREADS --oversubscribe ./${TARGET} -o $orden 2>&1 >/dev/null | tail -n 2) #Similar al anterior pero se capturan las últimas dos líneas en vez de solo una, para capturar el tamaño.
+            resultado=$(mpirun -np $THREADS --oversubscribe ./../cpp_y_ejecutables/${TARGET} -o $orden 2>&1 >/dev/null | tail -n 2) #Similar al anterior pero se capturan las últimas dos líneas en vez de solo una, para capturar el tamaño.
             size=$(echo "$resultado" | awk 'NR==1') #Extrae la primera línea del resultado.
             time=$(echo "$resultado" | awk 'NR==2') #Extrae la segunda línea del resultado.
 	        echo -n "$time," >> $timeOutput
