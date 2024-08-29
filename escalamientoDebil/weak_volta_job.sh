5694cd8a3f5e8cf1312d484bb9f4112bf506c081
CristianPe1 <cpenav@unal.edu.co>
2024-08-28 20:16:35 -0500

    added slurm sh

--- escalamientoDebil/weak_volta_job.sh
+++ escalamientoDebil/weak_volta_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=volta
-#SBATCH --output=resultados/weak_volta_%A_%a.out
-#SBATCH --error=resultados/weak_volta_%A_%a.err
-#SBATCH --partition=AMDRyzen7PRO5750G          # Asegúrate de especificar la partición correcta
-#SBATCH --cpus-per-task=1               # Número de CPUs por tarea
-#SBATCH --exclusive                     # Asegura que no compartas el nodo con otros trabajos
-#SBATCH --array=5-10         
+#SBATCH --output=resultados/slurm/volta_%A_%a.out
+#SBATCH --error=resultados/slurm/volta_%A_%a.err
+#SBATCH --array=1-6
+#SBATCH --nodelist=sala23
+#SBATCH --exclusive
+#SBATCH --partition=AMDRyzen7PRO5750G
 
 # Obtenemos el valor de ORDER a partir del índice del array
 ORDER=$SLURM_ARRAY_TASK_ID
