133a0e07311bc12b0ba38c214c44f187a4a193a2
svargasro <svargasro@unal.edu.co>
2024-08-31 19:28:30 -0400

    strongScaling of ex39p finished

--- escalamientoFuerte/scaling.sh
+++ escalamientoFuerte/scaling.sh
@@ -4,5 +4,5 @@
 #Ejecuta el .sh del escalamiento fuerte: ./strong_exp.sh Ejecutable MáximoOrden Threads Repeticiones
 
 #./strong_exp.sh ex1p 2 3 3 #Escalamiento fuerte ex1p
-./strong_exp.sh ex39p 2 3 3 #Escalamiento fuerte ex39p
+./strong_exp.sh ex39p 10 7 10 #Escalamiento fuerte ex39p
 #././strong_exp.sh ex1p 2 3 3#Escalamiento fuerte miniapp volta.
