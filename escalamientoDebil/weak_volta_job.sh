8046c09836704b70ac5bcd8b810bec7995c48ad6
CristianPe1 <cpenav@unal.edu.co>
2024-09-01 07:15:17 -0500

    Added order 10 and 11 in sala23

--- escalamientoDebil/weak_volta_job.sh
+++ escalamientoDebil/weak_volta_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=volta
 #SBATCH --output=resultados/slurm/volta_%A_%a.out
 #SBATCH --error=resultados/slurm/volta_%A_%a.err
-#SBATCH --array=1-10
+#SBATCH --array=10-13
 #SBATCH --nodelist=sala23
 #SBATCH --exclusive
 #SBATCH --partition=AMDRyzen7PRO5750G
