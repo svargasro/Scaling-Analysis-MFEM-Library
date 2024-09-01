7d16f933b3ea41fbd363d67cc659097d121a215e
Einsthephen <79174656+Einsthephen@users.noreply.github.com>
2024-08-31 22:19:59 -0500

    Comentado scaling.sh

--- scaling.sh
+++ scaling.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 cd ./escalamientoDebil
-#Ejecuta escalamiento débil para los ejemplos indicando: (ejecutable orden #threads #repetciciones)
-bash weak_exp.sh ex1p 2 3 3   #Ejemplo 1 version paralela
-#bash weak_exp.sh ex39p 2 4 3  #Ejemplo 39 version paralela
-#/escalamientoDebil/weak_volta.sh
+#En las siguientes 3 líneas se ejecuta el escalamiento débil para los ejemplos indicando: (ejecutable #orden #threads #repeticiones)
+bash weak_exp.sh ex1p 2 3 3   #Se ejecuta el ejemplo 1 version paralela del escalamiento débil para el orden, thread y repeticiones dadas. 
+#bash weak_exp.sh ex39p 2 4 3  #Se ejecuta el ejemplo 39 version paralela del escalamiento débil para el orden, thread y repeticiones dadas. 
+#/escalamientoDebil/weak_volta.sh # Se ejecuta el escalamiento débil para la miniapp Volta 
 
-#Construir gráfica de uso de memoria RAM
-#bash monitor_memoria.sh ex1p 2 1 0.001
-#
+#Construir gráfica de uso de memoria RAM. Aquí se ejecuta el script que extrae el consumo de memoria RAM generado por la ejecución del programa. Indicando: (ejecutable #orden #threads #Intervalo de medición medido en segundos)
+#Activar la línea anterior permite generar gráficas de RAM MÁXIMA VS Thread y RAM VS Tiempo
+#bash monitor_memoria.sh ex1p 2 1 0.001 
 cd ./../escalamientoFuerte/
-#Ejecuta el .sh del escalamiento fuerte: ./strong_exp.sh Ejecutable MáximoOrden Threads #Repeticiones
+#Ejecuta el .sh del escalamiento fuerte: ./strong_exp.sh: (Ejecutable #MáximoOrden #Threads #Repeticiones)
 
-./strong_exp.sh ex1p 2 3 3 #Escalamiento fuerte ex1p
-#./strong_exp.sh ex39p 2 3 3 #Escalamiento fuerte ex39p
-#././strong_exp.sh ex1p 2 3 3#Escalamiento fuerte miniapp volta.
+./strong_exp.sh ex1p 2 3 3 #Se ejecuta el ejemplo 1 version paralela del escalamiento fuerte para el orden, thread y repeticiones dadas.
+#./strong_exp.sh ex39p 2 3 3 #Se ejecuta el ejemplo 39 version paralela del escalamiento fuerte para el orden, thread y repeticiones dadas.
+#././strong_exp.sh ex1p 2 3 3#Se ejecuta el escalamiento fuerte para la miniapp Volta 
