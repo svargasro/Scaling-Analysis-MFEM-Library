444b1aefd1fe61912d80ca1868c43a1fbacd7a22
AFmartinezF <anmartinezf@unal.edu.co>
2024-09-01 10:13:12 -0500

    Added documentation

--- escalamientoDebil/weakPlot.py
+++ escalamientoDebil/weakPlot.py
@@ -6,55 +6,55 @@ from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.ticker import MaxNLocator
 import os
 
+# Configuración de argumentos de línea de comandos
+# input_file: Nombre del archivo CSV que contiene los datos de tiempo
 parser = argparse.ArgumentParser(description="Generar gráficas de SpeedUp y Eficiencia")
 parser.add_argument('input_file', type=str, help='Nombre del archivo de texto con los datos')
 args = parser.parse_args()
 
+# Leer los tiempos desde el archivo CSV y eliminar la primera columna (nombres de threads)
 time = pd.read_csv(f'{args.input_file}',header=None)
-
 time = time.drop(time.columns[0], axis=1)
 
+nThreads = time.shape[0] # Número de threads (filas en el CSV)
 
+speedup = time.iloc[0,:] /time.iloc[:,:] # Calculo del SpeedUp: T(1 hilo) / T(N hilos)
 
-nThreads = time.shape[0]
-#SpeedUp= T(1Thread)/T(Nthreads)
-
-speedup = time.iloc[0,:] /time.iloc[:,:]
-
+# Calculo de la Eficiencia: SpeedUp / N hilos
 threadsArray = np.arange(1,nThreads+1)
 efficiency = speedup/(threadsArray.reshape(-1, 1))
 
+# Calcular medias y desviaciones estándar para SpeedUp y Eficiencia
 mean_speedup = speedup.mean(axis=1)
 std_speedup = speedup.std(axis=1)
 mean_efficiency = efficiency.mean(axis=1)
 std_efficiency = efficiency.std(axis=1)
 
-# Extract the file name without the extension
+# Obtener el nombre base del archivo sin extensión
 file_name = os.path.splitext(os.path.basename(args.input_file))[0]
-print('file_name:', file_name)
 
-# Extract the target and order from the file name
+# Extraer el objetivo (target) y el orden (order) del nombre del archivo
 target = file_name.split('_')[1]
 order = file_name.split('_')[3]
 
 print('target:', target)
 print('order:', order)
 
-# Leer datos desde el archivo metrics.txt
 
-
+# Ruta para el archivo PDF de salida
 output='./resultados/graficas/weak_'+ str(target) +'_order_' + str(order) +'.pdf'
 
+# Configurar márgenes de error (tres veces la desviación estándar)
 errorbarSpeedUp = 3*std_speedup
 errorbarEfficiency = 3*std_efficiency
+
+# Estilo de las gráficas
 plt.style.use('ggplot')
 
-
-
-
+# Guardar las gráficas en un archivo PDF
 with PdfPages(output) as pdf:
-    # SpeedUp Plot
-
+        
+    # Gráfica de SpeedUp
     plt.plot([0, len(threadsArray)+0.2], [0, len(threadsArray)+0.2], color='black')  #Ideal SpeedUp
     plt.errorbar(threadsArray, mean_speedup, yerr= errorbarSpeedUp, fmt='go', ecolor='black', markersize=3, label='SpeedUp')
     plt.xlabel('nThreads')
@@ -64,12 +64,12 @@ with PdfPages(output) as pdf:
     plt.ylim(0, len(threadsArray)+0.2)
     plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
     plt.legend()
-    pdf.savefig()
+    pdf.savefig() # Guardar la gráfica en el PDF
     plt.close()
 
-    #Efficiency Plot
-    plt.plot([0, len(threadsArray)+0.2], [1, 1], color='red')  # Ideal Efficiency
-    plt.plot([0, len(threadsArray)+0.2], [0.6, 0.6], color='blue')  #Aceptable Efficiency
+    # Gráfica de Eficiencia
+    plt.plot([0, len(threadsArray)+0.2], [1, 1], color='red') # Línea ideal de Eficiencia
+    plt.plot([0, len(threadsArray)+0.2], [0.6, 0.6], color='blue')  # Línea de eficiencia aceptable
     plt.errorbar(threadsArray, mean_efficiency, yerr= errorbarEfficiency, fmt='go', ecolor='black', markersize=3, label='Efficiency')
     plt.xlabel('nThreads')
     plt.ylabel('Efficiency')
@@ -78,5 +78,6 @@ with PdfPages(output) as pdf:
     plt.ylim(0, 1.1)
     plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
     plt.legend()
-    pdf.savefig()
+    pdf.savefig() # Guardar la gráfica en la segunda página PDF
     plt.close()
+
--- scaling.sh
+++ scaling.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 cd ./escalamientoDebil
-#Ejecuta escalamiento débil para los ejemplos indicando: (ejecutable orden #threads #repetciciones)
-bash weak_exp.sh ex1p 2 3 3   #Ejemplo 1 version paralela
-#bash weak_exp.sh ex39p 2 4 3  #Ejemplo 39 version paralela
+#Ejecuta el .sh del escalamiento débil: ./weak_exp.sh Ejecutable Orden Máximo#Threads #Repeticiones
+./weak_exp.sh ex1p 2 4 5   #Ejemplo 1 version paralela
+./weak_exp.sh ex39p 2 4 5  #Ejemplo 39 version paralela
 #/escalamientoDebil/weak_volta.sh
 
 #Construir gráfica de uso de memoria RAM
-bash monitor_memoria.sh ex1p 2 1 0.001
-#
-cd ./../escalamientoFuerte/
-#Ejecuta el .sh del escalamiento fuerte: ./strong_exp.sh Ejecutable MáximoOrden Threads #Repeticiones
+./monitor_memoria.sh ex1p 10 14 0.001
+./monitor_memoria.sh ex39p 8 7 0.001
 
+cd ./../escalamientoFuerte/
+
+#Ejecuta el .sh del escalamiento fuerte: ./strong_exp.sh Ejecutable MáximoOrden Threads #Repeticiones
 ./strong_exp.sh ex1p 2 3 3 #Escalamiento fuerte ex1p
 #./strong_exp.sh ex39p 2 3 3 #Escalamiento fuerte ex39p
 #././strong_exp.sh ex1p 2 3 3#Escalamiento fuerte miniapp volta.
